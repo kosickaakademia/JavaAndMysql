104a6385b1b8e2089e75e030cc03e9a9a94b4f56
kosickaakademia <roland.onofrej@kosickaakademia.sk>
2021-02-02 11:01:13 +0100

    Added insert operation: insertNewCity()

--- src/sk/kosickaakademia/mysql/Database.java
+++ src/sk/kosickaakademia/mysql/Database.java
@@ -78,4 +78,53 @@ public class Database {
         return countryInfo;
     }
 
+    public String getCountryCode(String name)  {
+        if(name==null || name.equalsIgnoreCase(""))
+            return null;
+        try {
+            Connection con = getConnection();
+            String query = "SELECT Code FROM country WHERE Name LIKE ?";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, name);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                String code = rs.getString("Code");
+                con.close();
+                return code;
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void insertCity(City newCity) {
+
+        String country = newCity.getCountry();
+        String code3 = getCountryCode(country);
+        if(code3==null){
+            System.out.println("Warning! Country "+country+" does not exist! ");
+        }
+        else{
+            newCity.setCode3(code3);
+            String query = "INSERT INTO city (Name, CountryCode, District, Info) " +
+                    "VALUES( ?,?,?,?) ";
+            try {
+                Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(query);
+                ps.setString(1,newCity.getName());
+                ps.setString(2,newCity.getCode3());
+                ps.setString(3,newCity.getDistrict());
+                String json="{ \"Population\":"+newCity.getPopulation()+"}";
+                ps.setString(4,json);
+                boolean result= ps.execute();
+                System.out.println("Result: "+result);
+                con.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
