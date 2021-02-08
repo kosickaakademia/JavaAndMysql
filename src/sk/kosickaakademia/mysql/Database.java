9a3aff4221e6765e95da905d628d20ede92749c0
kosickaakademia <roland.onofrej@kosickaakademia.sk>
2021-02-08 11:44:38 +0100

    insertNewMonument

--- src/sk/kosickaakademia/mysql/Database.java
+++ src/sk/kosickaakademia/mysql/Database.java
@@ -127,4 +127,58 @@ public class Database {
             }
         }
     }
+
+    public int existCity(String code3, String cityName){
+        if(code3==null || cityName == null || code3.equals("") || cityName.equals(""))
+            return -1;
+
+        String query = "SELECT id FROM city WHERE CountryCode LIKE ? AND name LIKE ? ";
+        Connection con=null;
+        try {
+             con = getConnection();
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, code3);
+            ps.setString(2, cityName);
+            ResultSet rs = ps.executeQuery();
+            System.out.println(ps);
+
+            if(rs.next()){
+                int id = rs.getInt("id");
+                con.close();
+                return id;
+            }else{
+                con.close();
+                return -1;
+            }
+
+        }catch(Exception e){
+
+        }finally {
+
+        }
+        return -1; // mesto neexistuje
+    }
+
+    public boolean insertNewMonument( String code3, String city, String name ){
+        if(name==null || name.equals(""))
+            return false;
+
+        int cityId=existCity(code3, city);
+        System.out.println(cityId);
+        if(cityId==-1)
+            return false;
+        // vlozenie pamiatky
+        String query = "INSERT INTO monument(name, city) VALUES (?, ?)";
+        try {
+            Connection con = getConnection();
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1,name);
+            ps.setInt(2,cityId);
+            ps.executeUpdate();
+            con.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return true;
+    }
 }
--- src/sk/kosickaakademia/mysql/Main.java
+++ src/sk/kosickaakademia/mysql/Main.java
@@ -18,5 +18,7 @@ public class Main {
 
         City newCity = new City("Sliac",289, "Bystricke okolie","Slovakia");
         database.insertCity(newCity);
+
+        database.insertNewMonument("FRA" , "Paris","Eiffel Tower");
     }
 }
